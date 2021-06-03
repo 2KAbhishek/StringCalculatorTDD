8b536099471daaa0f7cbf342ab53901bf066cffa
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-03 17:53:25 +0530

    Add code to pass empty string test

--- src/main/java/iam2KAbhishek/StringCalculatorTDD/StringCalculator.java
+++ src/main/java/iam2KAbhishek/StringCalculatorTDD/StringCalculator.java
@@ -9,4 +9,12 @@ package iam2KAbhishek.StringCalculatorTDD;
  */
 public class StringCalculator {
 
+	/**
+	 * Add numbers present in input string
+	 * @param numbers
+	 * @return sum of numbers
+	 */
+	public int add(String numbers) {
+		return 0;
+	}
 }
