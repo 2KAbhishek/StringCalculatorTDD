d83d95e46095e436ec81684e425e58c8a2b145b1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-03 21:01:16 +0530

    Add code for negative number test passing

--- src/main/java/iam2KAbhishek/StringCalculatorTDD/StringCalculator.java
+++ src/main/java/iam2KAbhishek/StringCalculatorTDD/StringCalculator.java
@@ -3,12 +3,14 @@
  */
 package iam2KAbhishek.StringCalculatorTDD;
 
+import java.util.Arrays;
+
 /**
  * @author 2KAbhishek Main string calculator class
  */
 public class StringCalculator {
 
-	private static final String delimeter = "\\D";
+	private static final String delimeter = "[^0-9-]";
 
 	/**
 	 * Add numbers present in input string
@@ -16,18 +18,40 @@ public class StringCalculator {
 	 * @param numbers
 	 * @return sum of numbers
 	 */
-	public int add(String numbers) {
+	public int add(String numbers) throws Exception {
 		String[] nums = numbers.split(delimeter);
-		int sum = 0;
-
+		int sum = 0, intNum = 0;
 		for (String num : nums) {
 			try {
-				sum += Integer.parseInt(num);
-			} catch (Exception e) {
+				intNum = Integer.parseInt(num);
+				if (intNum < 0)
+					throw new Exception("Negatives not allowed" + Arrays.toString(getNegative(nums)));
+				else
+					sum += intNum;
+			} catch (NumberFormatException e) {
 				sum += 0;
 			}
 		}
 
 		return sum;
+	}
+
+	/**
+	 * @param nums
+	 * @return array of negative numbers
+	 */
+	private int[] getNegative(String[] nums) {
+		int[] negatives = new int[nums.length];
+		int intNum = 0, count = 0;
+		for (String num : nums) {
+			try {
+				intNum = Integer.parseInt(num);
+				if (intNum < 0)
+					negatives[count++] = intNum;
+			} catch (NumberFormatException e) {
+				continue;
+			}
+		}
+		return negatives;
 	}
 }
