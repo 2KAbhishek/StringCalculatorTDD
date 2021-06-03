0661acc350e99d9f6a58a1bb3b341af3c6994655
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-03 13:08:31 +0530

    Add before test init

--- src/test/java/iam2KAbhishek/StringCalculatorTDD/StringCalculatorTest.java
+++ src/test/java/iam2KAbhishek/StringCalculatorTDD/StringCalculatorTest.java
@@ -2,11 +2,18 @@ package iam2KAbhishek.StringCalculatorTDD;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.annotations.BeforeTest;
+
 /**
  * Unit tests for StringCalculator.
  */
 public class StringCalculatorTest {
 
 	private StringCalculator strCalc;
+
+	@BeforeTest
+	public void init() {
+		strCalc = new StringCalculator();
+	}
 
 }
